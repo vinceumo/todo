6abe4d5598323e3007ed46c9bd617dce3f4f2407
Vincent Humeau <hello@vincent-humeau.com>
2018-07-15 18:15:20 +0100

    update sw path for gh pages

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 // Initialize required variables - Cache Assets
 var shellCacheName = 'pwa-todo-v1';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/content/css/screen.min.css',
-  '/content/js/app.js',
+  './',
+  './index.html',
+  './content/css/screen.min.css',
+  './content/js/app.js',
   'https://cdn.jsdelivr.net/npm/vue',
   'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css'
 ];
